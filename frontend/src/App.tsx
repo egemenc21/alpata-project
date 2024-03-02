a80cca6616739233aeb5036fe8083f0cd4724aca
Egemen <egemenc2101@gmail.com>
2024-03-02 20:32:14 +0300

    react router dom configured

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
+import {Route, Routes} from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <div className='text-[#9b00009f]'>
-         aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-        </div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route path="/" element={<div>SignIn</div>} />
+        <Route path="/register" element={<div>Register</div>} />
+        <Route path="/dashboard" element={<div>CRUD</div>} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
